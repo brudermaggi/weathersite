39e12ff3abcd2708b6d9691987826b0067dfa5f3
stechmo <moritzstecher@gmx.de>
2024-01-29 18:09:32 +0100

    correcting mistakes

--- weathersite/weatherapp/views.py
+++ weathersite/weatherapp/views.py
@@ -1,5 +1,4 @@
 from django.http import HttpResponse
-<<<<<<< HEAD
 from django.shortcuts import render
 
 
@@ -9,9 +8,3 @@ def index(request):
 # Hier müssen wir den Zugriff auf die API realisieren, evtl durch eine Methode
 # Hier wird unter anderem in einzelnen Methoden eine HTTP-Anfrage angenommen und bearbeitet
 # Das bedeutet wir verwalten hier welche Informationen im Browser angezeigt werden und verarbeiten Benutzer-Eingaben
-=======
-
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
->>>>>>> origin/main
