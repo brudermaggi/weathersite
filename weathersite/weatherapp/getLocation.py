f0fe6910e920f65900cbf62147118db2c0d7a937
brudermaggi <152855041+brudermaggi@users.noreply.github.com>
2024-02-05 16:32:32 +0100

    Update getLocation.py
    update the getLocation Method

--- weathersite/weatherapp/getLocation.py
+++ weathersite/weatherapp/getLocation.py
@@ -1,21 +1,15 @@
 import requests
-
+import geocoder
 
 def get_ip():
     response = requests.get('https://api64.ipify.org?format=json').json()
     return response["ip"]
 
 
-def get_location():
-    ip_address = get_ip()
-    response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
-    location_data = {
-        "ip": ip_address,
-        "latitude": response.get("latitude"),
-        "longitude": response.get("longitude")
-    }
-    print(response)
-    return location_data
+def get_location(ip):
+    loc = geocoder.ip(ip)
+    return loc.latlng, loc.city
+    
 
-
-print(get_location())
+ip = get_ip()
+print(get_location(ip))
