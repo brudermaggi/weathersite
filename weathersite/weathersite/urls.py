421d9e29fae3d4e4ee02ed4dc980ee5238bc245b
stechmo <moritzstecher@gmx.de>
2024-02-05 15:17:18 +0100

    Adding a Folder for Static-Files and move some files in the right directory

--- weathersite/weathersite/urls.py
+++ weathersite/weathersite/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-
-urlpatterns = [
+urlpatterns = ([
     path('',include('weatherapp.urls')),
     path('admin/', admin.site.urls),
-]
+])
+
+urlpatterns += staticfiles_urlpatterns()
