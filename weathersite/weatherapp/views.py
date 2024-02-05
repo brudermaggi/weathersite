f3f511e2975b8e3d31406542eb20afe04aad8763
stechmo <moritzstecher@gmx.de>
2024-02-05 16:29:01 +0100

    API-Abfragen in views.py einbauen

--- weathersite/weatherapp/views.py
+++ weathersite/weatherapp/views.py
@@ -1,10 +1,42 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import requests
 
+APIkey = "c4bb3958e6d110d2e0036c0027da9ad0"
 
 def index(request):
-    return render(request, 'weatherapp/weather_content.html')
+    location = get_location()
+    weather = get_current_weather(location["latitude"], location["longitude"], APIkey)
+    context = {
+        'weather': weather['weather'],
+        'main': weather['main']
+    }
+    return render(request, 'weatherapp/weather_content.html', context)
 
-# Hier müssen wir den Zugriff auf die API realisieren, evtl durch eine Methode
-# Hier wird unter anderem in einzelnen Methoden eine HTTP-Anfrage angenommen und bearbeitet
-# Das bedeutet wir verwalten hier welche Informationen im Browser angezeigt werden und verarbeiten Benutzer-Eingaben
+#hier wird der jetzige Standort ausfindig gemacht
+
+def get_ip():
+    response = requests.get('https://api64.ipify.org?format=json').json()
+    return response["ip"]
+
+def get_location():
+    ip_address = get_ip()
+    response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
+    location_data = {
+        "ip": ip_address,
+        "latitude": response.get("latitude"),
+        "longitude": response.get("longitude")
+    }
+    print(response)
+    return location_data
+
+#hier wird das Wetter am gesuchten Standort ausfindig gemacht
+
+def get_current_weather(lat, lon, Key):
+    response  = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={Key}&units=metric").json()
+
+    current_weather = {
+        "weather": response.get("weather"),
+        "main": response.get("main")
+    }
+    return current_weather
