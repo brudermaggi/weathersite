5c264f0e60a5a018dfc5b1ff6f89e18f0542fe6a
stechmo <moritzstecher@gmx.de>
2024-02-09 21:35:54 +0100

    For-Schleife in Template mit neuem return-Wert von get_weather_forecast

--- weathersite/weatherapp/views.py
+++ weathersite/weatherapp/views.py
@@ -88,6 +88,7 @@ def get_current_weather(lat, lon, Key):
     }
     return current_weather
 
+
 def get_weather_forecast(lat, lon, Key):
     response = requests.get(
         f"https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={Key}&units=metric").json()
@@ -97,6 +98,7 @@ def get_weather_forecast(lat, lon, Key):
     date_list = []
     weather_list = []
 
+    # Unsorted Forecast Dict
     for i in range(0, 40):
         date_list.append(response["list"][i]["dt_txt"])
         temp_list.append(response["list"][i]["main"]["temp"])
@@ -114,4 +116,58 @@ def get_weather_forecast(lat, lon, Key):
         "weather_list": weather_list,
         "rain_list": rain_list
     }
-    return forecast
+
+    # Sorted forecast list
+    sorted_forecast = []
+
+    for i in range(0, 40):
+        sorted_forecast.append(response["list"][i]["dt_txt"])
+        sorted_forecast.append(response["list"][i]["main"]["temp"])
+        sorted_forecast.append(response["list"][i]["weather"][0]["main"])
+
+        if response["list"][i]["weather"][0]["main"] == "Rain":
+            sorted_forecast.append(response["list"][i]["rain"]["3h"])
+        else:
+            sorted_forecast.append(0)
+        sorted_forecast.append(i)
+        sorted_forecast.append("Iteration")
+        print("Soerted iteration", i)
+
+    # Sorted forecast dict
+    sorted_list = []
+    sorted_forecast2 = {}
+
+    for i in range(0, 40):
+        sorted_list.clear()
+        sorted_list.append(response["list"][i]["dt_txt"])
+        sorted_list.append(response["list"][i]["main"]["temp"])
+        sorted_list.append(response["list"][i]["weather"][0]["main"])
+
+        if response["list"][i]["weather"][0]["main"] == "Rain":
+            sorted_list.append(response["list"][i]["rain"]["3h"])
+        else:
+            sorted_list.append(0)
+
+        sorted_forecast2[i] = sorted_list
+
+    # Sorted forecast list in list
+    sorted_forecast_list_in_list = []
+    sorted_forecast_main_list = []
+
+    for i in range(0, 40):
+        sorted_forecast_list_in_list.clear()
+        sorted_forecast_list_in_list.append(response["list"][i]["dt_txt"])
+        sorted_forecast_list_in_list.append(response["list"][i]["main"]["temp"])
+        sorted_forecast_list_in_list.append(response["list"][i]["weather"][0]["main"])
+
+        if response["list"][i]["weather"][0]["main"] == "Rain":
+            sorted_forecast_list_in_list.append(response["list"][i]["rain"]["3h"])
+        else:
+            sorted_forecast_list_in_list.append(0)
+
+        sorted_forecast_main_list.append(sorted_forecast_list_in_list)
+
+    # sorted_forecast follows this Syntax:
+    # [Date and Time, temperatur, weather Type, precipitation, iteration, Brakeword]
+
+    return sorted_forecast_main_list
